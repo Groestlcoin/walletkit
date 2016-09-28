ccfbcd19328ebaf2d4a80bd2bf32d3d5dbd22ffd
HashEngineering <hashengineeringsolutions@gmail.com>
2016-09-27 20:31:50 -0700

    Groestlcoin Hash

--- hashgroestl.h
+++ hashgroestl.h
@@ -2,30 +2,10 @@
 // Copyright (c) 2009-2012 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-#ifndef BITCOIN_HASH_H
-#define BITCOIN_HASH_H
-
-#include "uint256.h"
-#include "sph_groestl.h"
-#include <vector>
+#ifndef GROESTLCOIN_HASH_H
+#define GROESTLCOIN_HASH_H
 
 
-template<typename T1>
-inline uint256 HashGroestl(const T1 pbegin, const T1 pend)
-{
-    sph_groestl512_context  ctx_gr[2];
-    static unsigned char pblank[1];
-    uint512 hash[2];
-	
-    sph_groestl512_init(&ctx_gr[0]);
-    sph_groestl512 (&ctx_gr[0], (pbegin == pend ? pblank : static_cast<const void*>(&pbegin[0])), (pend - pbegin) * sizeof(pbegin[0]));
-    sph_groestl512_close(&ctx_gr[0], static_cast<void*>(&hash[0]));
-	
-	sph_groestl512_init(&ctx_gr[1]);
-	sph_groestl512(&ctx_gr[1],static_cast<const void*>(&hash[0]),64);
-	sph_groestl512_close(&ctx_gr[1],static_cast<void*>(&hash[1]));
-	
-    return hash[1].trim256();
-}
+void HashGroestl(void * buf, const void * pbegin, int len);
 
 #endif
