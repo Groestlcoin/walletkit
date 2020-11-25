1c9ad84f57e5961d56b2f3749db6065a020aefc1
HashEngineering <hashengineeringsolutions@gmail.com>
2020-11-25 14:01:24 -0800

    add publish.sh

---
+++ WalletKitJava/publish.sh
@@ -0,0 +1,6 @@
+./gradlew :crypto:assemble
+./gradlew :corecrypto-android:assemble
+./gradlew :corenative-android:assemble
+./gradlew :crypto:publishToMavenLocal
+./gradlew :corecrypto-android:publishToMavenLocal
+./gradlew :corenative-android:publishToMavenLocal
